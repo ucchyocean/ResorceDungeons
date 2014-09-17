883cda778596e0c017108095cefc14df741d3b6c
thekarura <karura2310@yahoo.co.jp>
2014-09-17 17:39:45 +0900

    自然生成のみ固定
    他の変更点としてワールド条件式を変更

--- src/main/java/com/thekarura/bukkit/plugin/resorcedungons/listener/RDEntityListener.java
+++ src/main/java/com/thekarura/bukkit/plugin/resorcedungons/listener/RDEntityListener.java
@@ -1,10 +1,10 @@
 package com.thekarura.bukkit.plugin.resorcedungons.listener;
 
-import org.bukkit.Bukkit;
 import org.bukkit.entity.EntityType;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.CreatureSpawnEvent;
+import org.bukkit.event.entity.CreatureSpawnEvent.SpawnReason;
 
 import com.thekarura.bukkit.plugin.resorcedungons.ResorceDungeons;
 
@@ -16,68 +16,67 @@ public class RDEntityListener implements Listener {
 		this.instance = resorcedungeons;
 	}
 	
-	//ジャングル地下ダンジョン用スポーンレート
-	@EventHandler
+	//特殊スポーンレート
+	@EventHandler(ignoreCancelled = true)
 	public void JungleCaveEntityChenge(CreatureSpawnEvent event){
 		
 		//ダンジョンワールドのみ有効化
-		if (event.getEntity().getWorld() == Bukkit.getWorld(instance.getConfigs().getDungeonWorld())){
+		if (event.getEntity().getWorld().equals(instance.getConfigs().getDungeonWorld())){
 			
-			//バイオーム指定
-			switch (event.getLocation().getBlock().getBiome()){
-			
-			//ジャングルのみ
-			case JUNGLE:
-			case JUNGLE_HILLS:
+			//自然湧きのみ固定
+			if (event.getSpawnReason().equals(SpawnReason.NATURAL)){
 				
-				//EntityType検索
-				switch (event.getEntityType()){
+				//バイオーム指定
+				switch (event.getLocation().getBlock().getBiome()){
 				
-				//毒グモへ置換するモブ
-				case CREEPER:
-				case ENDERMAN:
-				case SLIME:
+				//ジャングルのみ
+				case JUNGLE:
+				case JUNGLE_HILLS:
 					
-					event.setCancelled(true);
+					//EntityType検索
+					switch (event.getEntityType()){
 					
-					//群生設定
-					for (int limit = 0; limit < 5 ; limit++){
-						event.getLocation().getWorld().spawnEntity(event.getLocation(), EntityType.CAVE_SPIDER);
+					//毒グモへ置換するモブ
+					case CREEPER:
+					case ENDERMAN:
+					case SLIME:
+						
+						event.setCancelled(true);
+						
+						//群生設定
+						for (int limit = 0; limit < 3 ; limit++){
+							event.getLocation().getWorld().spawnEntity(event.getLocation(), EntityType.CAVE_SPIDER);
+						}
+						
+					break;
+					//ゾンビのレートを蜘蛛に変更
+					case ZOMBIE:
+						
+						event.setCancelled(true);
+						event.getLocation().getWorld().spawnEntity(event.getLocation(), EntityType.SPIDER);
+						
+					break;
+					
+					//動物の湧を一部消します。
+					case COW:
+					case SHEEP:
+					case CHICKEN:
+					case PIG:
+						
+						event.setCancelled(true);
+						
+					default:break;
 					}
 					
 				break;
-				//ゾンビのレートを蜘蛛に変更
-				case ZOMBIE:
-					
-					event.setCancelled(true);
-					event.getLocation().getWorld().spawnEntity(event.getLocation(), EntityType.SPIDER);
-					
-				break;
-				
-				//動物の湧を一部消します。
-				case COW:
-				case SHEEP:
-				case CHICKEN:
-				case PIG:
-					
-					event.setCancelled(true);
-					
 				default:break;
+				
 				}
 				
-			break;
-			default:break;
-			
 			}
 			
 		}
 		
-	}
-	
-	//砂漠用スポーンレート
-	//@EventHandler
-	public void DessertEntityChenge(){
-		//TODO まだ
 	}
 	
 }
