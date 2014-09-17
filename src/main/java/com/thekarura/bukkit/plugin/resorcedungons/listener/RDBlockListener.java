800fa906a8e415d6a1456624231c351771e80ba4
ucchy <ocean163@hotmail.com>
2014-09-18 04:48:57 +0900

    RDBlockListenerの軽量化

--- src/main/java/com/thekarura/bukkit/plugin/resorcedungons/listener/RDBlockListener.java
+++ src/main/java/com/thekarura/bukkit/plugin/resorcedungons/listener/RDBlockListener.java
@@ -9,98 +9,163 @@ import org.bukkit.block.Sign;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.scheduler.BukkitRunnable;
 
 import com.thekarura.bukkit.plugin.resorcedungons.ResorceDungeons;
 import com.thekarura.bukkit.plugin.resorcedungons.manager.DungeonMossy;
 
 /**
  * ResorceDungeonsのブロック関連を扱う部分です。
- * 
+ *
  * @author karura
  */
 public class RDBlockListener implements Listener {
-	
+
 	// ++ Logger ++ //
 	public static final Logger log = ResorceDungeons.log;
 	private static final String logPrefix = ResorceDungeons.logPrefix;
 	private static final String msgPrefix = ResorceDungeons.msgPrefix;
-	
+
 	// ++ Instance ++ //
 	private ResorceDungeons instance = ResorceDungeons.getInstance();
-	
+
 	// ++ Constructor ++ //
 	public RDBlockListener(final ResorceDungeons plugin) {
 		this.instance = plugin;
 	}
-	
+
 	/**
-	 * 一定範囲内にプレイヤーが入ると反応する
-	 * トリガーブロック
-	 * @param event PlayerMoveEvent
+	 * プレイヤーがサーバーに参加したときに発生するイベント
+	 * @param event
 	 */
-	@EventHandler(ignoreCancelled = true)
-	public void onPlayerMove(PlayerMoveEvent event){
-		
+	@EventHandler(ignoreCancelled=true)
+	public void onPlayerJoin(PlayerJoinEvent event) {
+
+		// 参加したプレイヤーに、1秒毎に動作する非同期処理タイマーを仕掛ける。
 		final Player player = event.getPlayer();
 		
-		//ダンジョンワールドのみ有効化
-		if (player.getWorld().getName().equals(instance.getConfigs().getDungeonWorld())){
+		new BukkitRunnable() {
 			
-			//デバッグ中なのでこれは隠おきましょう
-			//クリエイティブモードであれば実行させない
-			//if (player.getGameMode().equals(GameMode.CREATIVE)){ return; }
+			// 1秒前に居たプレイヤーの位置 の保存用変数
+			private Location prev;
 			
 			//感知範囲(コンフィグで調整予定)
-			int radius = 50;
+			final int radius = 50;
 			
-			//プレイヤーの位置を検出します
-			World world = player.getWorld();
-			int x = player.getLocation().getBlockX();
-			int z = player.getLocation().getBlockZ();
-			int y = player.getLocation().getBlockY();
-			
-			//ループ処理で特定のブロックを探します
-			for (int i = x - radius ; i < x + radius ; i++ ){
-				for (int j = z - radius ; j < z + radius ; j++ ){
-					for (int y_ =  y - radius ; y_ < y + radius ; y_++ ){
+			@Override
+			public void run() {
+				
+				// もしプレイヤーがサーバーから抜けたなら、このタイマーを終了する
+				if ( !player.isOnline() ) {
+					cancel();
+					return;
+				}
+				
+				// プレイヤーがチェック対象ワールドにいないなら処理をしない
+				if ( !player.getWorld().getName().equals(instance.getConfigs().getDungeonWorld())) {
+					prev = null;
+					return;
+				}
+				
+				Location loc = player.getLocation();
+				if ( prev != null ) {
+					// 1秒前の位置キャッシュがある場合
+					
+					// x方向に移動した場合、"移動した分のブロックだけ"をチェックする
+					if ( loc.getBlockX() != prev.getBlockX() ) {
+						Location min = (loc.getBlockX() < prev.getBlockX() ) ? loc.clone() : prev.clone();
+						Location max = (loc.getBlockX() < prev.getBlockX() ) ? prev.clone() : loc.clone();
 						
-						/* コマンドブロックの上に立っている看板を探します
-						 * 図式
-						 * 
-						 * [POST]
-						 * [COMB]
-						 * 
-						 */
-						if(world.getBlockAt(i, y, j).getType() == Material.COMMAND
-						&& world.getBlockAt(i, y + 1, j).getType() == Material.SIGN_POST){
+						// 調査範囲は、min{x, y-50, z-50} から max{x, y+50, z+50}
+						min.add(0, -radius, -radius);
+						max.add(0, radius, radius);
+						
+						// サーチ
+						search(min, max);
+					}
+					
+					// y方向に移動した場合も同様。
+					if ( loc.getBlockY() != prev.getBlockY() ) {
+						Location min = (loc.getBlockY() < prev.getBlockY() ) ? loc.clone() : prev.clone();
+						Location max = (loc.getBlockY() < prev.getBlockY() ) ? prev.clone() : loc.clone();
+						
+						// 調査範囲は、min{x-50, y, z-50} から max{x+50, y, z+50}
+						min.add(-radius, 0, -radius);
+						max.add(radius, 0, radius);
+						
+						// サーチ
+						search(min, max);
+					}
+					
+					// z方向に移動した場合も同様。
+					if ( loc.getBlockZ() != prev.getBlockZ() ) {
+						Location min = (loc.getBlockZ() < prev.getBlockZ() ) ? loc.clone() : prev.clone();
+						Location max = (loc.getBlockZ() < prev.getBlockZ() ) ? prev.clone() : loc.clone();
+						
+						// 調査範囲は、min{x-50, y-50, z} から max{x+50, y+50, z}
+						min.add(-radius, -radius, 0);
+						max.add(radius, radius, 0);
+						
+						// サーチ
+						search(min, max);
+					}
+					
+				} else {
+					// 1秒前の位置キャッシュが無い場合、しかたがないので全範囲をサーチする
+					Location min = loc.clone().add(-radius, -radius, -radius);
+					Location max = loc.clone().add(radius, radius, radius);
+					search(min, max);
+					
+				}
+				
+				// 今回のサーチした位置を保存しておく
+				prev = loc;
+			}
+			
+			private void search(Location min, Location max) {
+				
+				//ループ処理で特定のブロックを探します
+				World world = min.getWorld();
+				for (int x = min.getBlockX(); x < max.getBlockX(); x++ ){
+					for (int y = min.getBlockY(); y < min.getBlockY(); y++ ){
+						for (int z = min.getBlockZ(); z < min.getBlockZ(); z++ ){
 							
-							//看板の情報を上記のSIGN_POSTに設定します。
-							Sign sign = (Sign) world.getBlockAt(i, y + 1, j).getState();
-							
-							//生成位置を決めるため位置情報を登録します。
-							Location loc = new Location(world, i, y, j);
-							
-							//[RDungeons]と書かれている場合の条件
-							if(sign.getLine(0).equals("[RDungeons]")){
+							/* コマンドブロックの上に立っている看板を探します
+							 * 図式
+							 *
+							 * [POST]
+							 * [COMB]
+							 *
+							 */
+							if(world.getBlockAt(x, y, z).getType() == Material.COMMAND
+							&& world.getBlockAt(x, y+1, z).getType() == Material.SIGN_POST){
 								
-								//MossyDungeonsを生成します
-								if (sign.getLine(1).equals("Mossy")){
+								//看板の情報を上記のSIGN_POSTに設定します。
+								Sign sign = (Sign) world.getBlockAt(x, y+1, z).getState();
+								
+								//生成位置を決めるため位置情報を登録します。
+								Location loc = new Location(world, x, y, z);
+								
+								//[RDungeons]と書かれている場合の条件
+								if(sign.getLine(0).equals("[RDungeons]")){
 									
-									remove(world, i, y, j);
-									new DungeonMossy().setDungeonMossy(loc);
-									
+									//MossyDungeonsを生成します
+									if (sign.getLine(1).equals("Mossy")){
+										
+										remove(world, x, y, z);
+										new DungeonMossy().setDungeonMossy(loc);
+									}
 								}
-								
 							}
 						}
 					}
 				}
 			}
-		}
-		
+		}.runTaskTimerAsynchronously(ResorceDungeons.getInstance(), 20, 20);
+		// ↑タスクを非同期で、20ticks(1秒)後から、20ticksごとにrunメソッドを実行する
 	}
-	
+
 	/**
 	 * 看板削除用メソッド
 	 * @param w
@@ -113,5 +178,5 @@ public class RDBlockListener implements Listener {
 			w.getBlockAt(i, y + y_, j).setType(Material.AIR);
 		}
 	}
-	
+
 }
